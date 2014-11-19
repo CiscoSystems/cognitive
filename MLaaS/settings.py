dc83296cef2d0609c9231ab16915cb615bf59553
Komei Shimamura <komei.t.f@gmail.com>
2014-11-18 19:46:28 -0800

    include jquery and bootstrap

--- MLaaS/settings.py
+++ MLaaS/settings.py
@@ -90,3 +90,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR+'/MLaaS/MLApp', "static"),
+)
+
