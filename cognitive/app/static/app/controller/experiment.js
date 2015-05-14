4e0e4e7afbf39a5253eac6445c06cdc8c3c4e683
Komei Shimamura <komei.t.f@gmail.com>
2015-05-14 12:59:27 -0700

    stop click event propagation from component
    - Add stopPropagation() into the function witch is invoked when
      components are clicked.

--- cognitive/app/static/app/controller/experiment.js
+++ cognitive/app/static/app/controller/experiment.js
@@ -94,8 +94,10 @@ cognitive.controller('ExperimentController', function (
             .animate({"left":0}, 700);
     };
 
-    $scope.select_none = function () {
+    $scope.clickNone = function () {
         $scope.closeDescription();
+        var current_node = $scope.getCurrentFocusNode()
+        current_node.focus = false;
     };
 
     $scope.existFocusedNodeOnCurrentWorkspace = function() {
@@ -134,6 +136,7 @@ cognitive.controller('ExperimentController', function (
     //};
 
     $scope.clickCognitiveNode = function (event, workspace_id, index) {
+        event.stopPropagation();
         var node = $scope.getNodeByWorkspaceAndIndex(workspace_id, index);
         if (node.focus) {
             node.focus = false;
